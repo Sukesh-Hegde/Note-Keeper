f987e4022632c7adafc983b3ae45006603cabdfc
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-08-21 18:47:52 +0530

    updated About section

--- src/components/About.js
+++ src/components/About.js
@@ -10,12 +10,13 @@ export default function About() {
 
       <main className="container my-5">
         <section className="mb-5">
-          <h2>Our Mission</h2>
+          <h2>Note-Keeper</h2>
           <p>
-            Our mission is to provide the best service to our customers by
-            leveraging our expertise and innovative solutions. We strive to
-            exceed expectations and foster long-term relationships built on
-            trust and satisfaction.
+            The Note-Keeper is a full-stack web application designed to provide
+            users with a secure and convenient way to manage their notes. Built
+            using the MERN stack (MongoDB, Express.js, React, Node.js), this
+            application allows users to create, update, delete, and share notes
+            with ease.
           </p>
         </section>
 
@@ -24,7 +25,7 @@ export default function About() {
           <div className="row">
             <div className="col-md-4 team-member text-center">
               <img
-                src="https://via.placeholder.com/150"
+                src="https://plus.unsplash.com/premium_photo-1672239496290-5061cfee7ebb?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="Team Member"
                 className="img-fluid mb-3"
               />
@@ -33,7 +34,7 @@ export default function About() {
             </div>
             <div className="col-md-4 team-member text-center">
               <img
-                src="https://via.placeholder.com/150"
+                src="https://media.istockphoto.com/id/1702492633/photo/beautiful-and-happy-smiling-asian-young-girl-from-nepal-giving-several-gestures-in-a-formal.jpg?s=1024x1024&w=is&k=20&c=d_94zs04XbfWaCPszisYD5uruE-MRYmRC8DmHsFu-js="
                 alt="Team Member"
                 className="img-fluid mb-3"
               />
@@ -42,7 +43,7 @@ export default function About() {
             </div>
             <div className="col-md-4 team-member text-center">
               <img
-                src="https://via.placeholder.com/150"
+                src="https://images.unsplash.com/photo-1534030347209-467a5b0ad3e6?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="Team Member"
                 className="img-fluid mb-3"
               />
@@ -59,7 +60,7 @@ export default function About() {
             to hear from you!
           </p>
           <p>
-            Email: <a href="mailto:info@example.com">info@example.com</a>
+            Email: <a href="mailto:info@example.com">sukeshhegde994591@gmail.com</a>
           </p>
           <p>Phone: (123) 456-7890</p>
         </section>
